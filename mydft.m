b1a9b961d66c6c5155d2d82b6317c73e61ab542a
Thooby <thobias.ivarsson@gmail.com>
2017-10-17 16:41:36 +0200

    Fixat

--- mydft.m
+++ mydft.m
@@ -1,5 +1,15 @@
 function z = mydft(y)
-% Compute the DFT of a vector y of length N
-% z k = 1/N sum {1=0}ˆ{N-1}y 1 exp(-2 pi k 1/N)
-N = length(y);
-omega = exp (2.*pi.*i./N)% here i is the imaginary unit
+    % Compute the DFT of a vector y of length N
+    % z_k = 1/N sum_{1=0}ˆ{N-1}y 1 exp(-2 pi k 1/N)
+    N = length(y);
+    omega = exp (2.*pi.*i./N);% here i is the imaginary unit
+    for n = 1:N
+        temp_sum = 0;
+        for j = 1:N
+            temp_sum = temp_sum + y(j).*omega.^(-(n-1)*(j-1));
+            z(n) = temp_sum .* (1/N);
+        end
+    end
+end
+            
+    
