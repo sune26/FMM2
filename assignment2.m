06b996643a5b9077bbb6c8899b9527caefc540c0
Thooby <thobias.ivarsson@gmail.com>
2017-10-17 21:13:14 +0200

    påbörjat c men den suger

---
+++ assignment2.m
@@ -0,0 +1,71 @@
+%b)
+
+clear all
+N = 2^4;
+for i = 0:N-1
+    x(i+1) = -pi+2*pi*i/N;
+end
+y = zeros(1,N);
+for j = 1:N
+    y(j) = 3+2*cos(50*x(j))+4*sin(120*x(j));
+end
+z = mydft(y);
+figure
+stem([0:N-1],real(z))
+title('Real part n = 2^4')
+figure
+stem([0:N-1],imag(z))
+title('Imaginary part n = 2^4')
+figure
+stem([0:N-1],abs(z))
+title('Amplitude n = 2^4')
+
+% N = 2^8
+
+clear all
+N = 2^8;
+for i = 0:N-1
+    x(i+1) = -pi+2*pi*i/N;
+end
+y = zeros(1,N);
+for j = 1:N
+    y(j) = 3+2*cos(50*x(j))+4*sin(120*x(j));
+end
+z = mydft(y);
+figure
+stem([0:N-1],real(z))
+title('Real part n = 2^8')
+figure
+stem([0:N-1],imag(z))
+title('Imaginary part n = 2^8')
+figure
+stem([0:N-1],abs(z))
+title('Amplitude n = 2^8')
+
+%c)
+
+clear all
+N = 2^8;
+for i = 0:N-1
+    x(i+1) = 2*pi*i/N;
+end
+y = zeros(1,N);
+for j = 1:N
+    y(j) = abs(cos(x(j)));
+end
+z = mydft(y);
+
+for i = 1:N
+    a(i) = real(z(i)) + real(z(N+1-i));% cosine coefficients calculated from the DFT z
+end
+aexact(1) = 2/pi;
+for k = 1:N/2
+    aexact(k+1) = -(4/pi)*(((-1)^k)/((4*k^2)-1));
+end
+% plot half number of cosine coefficients
+figure
+stem(a(1:N/2),'*')
+hold on
+% plot half exact Fourier cosine coefficients
+stem(aexact(1:N/2),'or')
+hold off
