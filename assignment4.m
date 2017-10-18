7d0dc1dd3db6f9c734aa8c6d6ef47e0cb24a2403
Thooby <thobias.ivarsson@gmail.com>
2017-10-18 19:46:33 +0200

    4 klar

--- assignment4.m
+++ assignment4.m
@@ -1,9 +1,18 @@
 clear all
-fid = fopen('filter.data','r'); % open filter.data
-Y = fscanf(fid,'%f','%f',[1 inf]); % read filter.data
+fid = fopen('filtre.data','r'); % open filter.data
+Y = fscanf(fid,'%f',[1 inf]); % read filter.data
 fclose(fid)
 figure
 plot(Y) % plot filter.data
-..
-if (abs(Z(j)) < ..) % cut-off low frequencies
-Z(j) = 0;
+Z = myfft(Y,length(Y));
+
+for j = 1:length(Z)
+    if (abs(Z(j)) < 0.02)
+        Z(j) = 0;
+    end
+end
+
+Y = real(myifft(Z,length(Z)));
+
+figure
+plot(Y)
