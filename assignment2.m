3c19252530330c7034de08c8b959c2cb02af9ddf
Johan <johan.viklund97@gmail.com>
2017-10-18 16:02:00 +0200

    Fixat c med fulhack

--- assignment2.m
+++ assignment2.m
@@ -53,19 +53,22 @@ y = zeros(1,N);
 for j = 1:N
     y(j) = abs(cos(x(j)));
 end
-z = mydft(y);
-
-for i = 1:N
-    a(i) = real(z(i)) + real(z(N+1-i));% cosine coefficients calculated from the DFT z
+z = mydft(y)
+a(1) = real(z(1)) + real(z(N));
+for i = 1:N-1
+    a(i+1) = real(z(1+i)) + real(z(N+1-i));% cosine coefficients calculated from the DFT z
+end
+for i = 1:N/2
+    a(i) = a(2*i-1);
 end
 aexact(1) = 2/pi;
 for k = 1:N/2
-    aexact(k+1) = -(4/pi)*(((-1)^k)/((4*k^2)-1));
+        aexact(k+1) = -(4/pi)*(((-1)^k)/((4*k^2)-1));
 end
 % plot half number of cosine coefficients
 figure
-stem(a(1:N/2),'*')
+stem(a(1:N/4),'*')
 hold on
 % plot half exact Fourier cosine coefficients
-stem(aexact(1:N/2),'or')
+stem(aexact(1:N/4),'or')
 hold off
